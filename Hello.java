88865b0ccb4c40c5eed461884d3e13baeba95af3
Gareth Andrews <Gareth.Andrews@chi.swan.ac.uk>
2018-07-05 14:41:23 +0100

    added new function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,25 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello hello");
+		
+		//killPC();
     }
+	
+	private void killPC() {
+		for (int i = 0; i < 1000; i++) {
+			System.out.println(i*123456789);
+		}
+	}
+	
+	public static int minFunction(int n1, int n2) {
+   		int min;
+		
+   		if (n1 > n2)
+      		min = n2;
+   		else
+      		min = n1;
+
+   		return min; 
+	}
+	
 }
