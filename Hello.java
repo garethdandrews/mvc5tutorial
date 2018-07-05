eebc1d2a414c53a8b1aceb9819d74532cc105831
oskarfigura <Oskar.Figura@chi.swan.ac.uk>
2018-07-05 14:36:41 +0100

    Added new method

--- Hello.java
+++ Hello.java
@@ -2,4 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello hello");
     }
+
+    public void testMthd(String msg) {
+        int n = 5;
+        System.out.println(n);
+    }
 }
